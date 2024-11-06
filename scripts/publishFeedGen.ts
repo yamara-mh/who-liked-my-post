fa66328b836c81a483c92db2e055ac1ca0a70f63
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-07 00:39:48 +0900

    Update publishFeedGen.ts

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -7,57 +7,16 @@ import { ids } from '../src/lexicon/lexicons'
 const run = async () => {
   dotenv.config()
 
+  const handle = process.env.FEEDGEN_PUBLISHER_IDENTIFIER
+  const password = process.env.FEEDGEN_APP_PASSWORD
+  const recordName = 'test'
+  const displayName = 'Test Feed'
+  const description = 'This is a test feed.'
+  const avatar = '../icon.png'
+
   if (!process.env.FEEDGEN_SERVICE_DID && !process.env.FEEDGEN_HOSTNAME) {
     throw new Error('Please provide a hostname in the .env file')
   }
-
-  const answers = await inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'handle',
-        message: 'Enter your Bluesky handle:',
-        required: true,
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Enter your Bluesky password (preferably an App Password):',
-      },
-      {
-        type: 'input',
-        name: 'service',
-        message: 'Optionally, enter a custom PDS service to sign in with:',
-        default: 'https://bsky.social',
-        required: false,
-      },
-      {
-        type: 'input',
-        name: 'recordName',
-        message: 'Enter a short name or the record. This will be shown in the feed\'s URL:',
-        required: true,
-      },
-      {
-        type: 'input',
-        name: 'displayName',
-        message: 'Enter a display name for your feed:',
-        required: true,
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Optionally, enter a brief description of your feed:',
-        required: false,
-      },
-      {
-        type: 'input',
-        name: 'avatar',
-        message: 'Optionally, enter a local path to an avatar that will be used for the feed:',
-        required: false,
-      },
-    ])
-
-  const { handle, password, recordName, displayName, description, avatar, service } = answers
 
   const feedGenDid =
     process.env.FEEDGEN_SERVICE_DID ?? `did:web:${process.env.FEEDGEN_HOSTNAME}`
